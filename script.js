4da1e0e71ad11ac6605236b0819439027bf039e4
aje-johns <aje.deploy@gmail.com>
2022-12-04 17:30:20 +0530

    TEST 1

--- script.js
+++ script.js
@@ -4,36 +4,58 @@ const rulebookInput = document.getElementById("rbInput");
 const queryDesc = document.getElementById("queryDesc");
 const auditorEmail = document.getElementById("auditorEmail");
 const axisShare = document.getElementById("axisShare");
-const op = document.getElementById("output");
 
 link.onclick = () => {
   const underWriterEmailId = emailAddress.value;
   const rulebookInputValue = rulebookInput.value;
   const queryDescription = queryDesc.value;
-  let assuredName = "Test Assured";
-  let submissionId = "Test SID";
   let axisShareValue = axisShare.value;
   let auditorEmailAddress = auditorEmail.value;
   let ccArray = ["a@gmail.com", "b@gmail.com"];
 
+  let subIdRegex = /S-[0-9]*-[0-9]*/gm;
+  let policyNumberRegex = /[0-9]*\/[0-9]*\/ | [0-9]*\/[0-9]*\/[0-9]*\/[0-9]*\//;
+
+  let policyNumberList = rulebookInputValue.match(policyNumberRegex);
+  let submissionId = rulebookInputValue.match(subIdRegex);
+  let assuredName = getStringBetween(
+    "ASSURED:",
+    "BROKER CONTACT:",
+    rulebookInputValue
+  );
+
   let subject = `${assuredName} /\ ${submissionId} /\ ${axisShareValue}`;
   let cc = [...ccArray, auditorEmailAddress];
-  let uwFirstName = "";
+  let uwFirstName = capitalizeName(getName(underWriterEmailId).firstName);
   let newLine = "%0a%0d%0a";
-  let policyNumberList = [];
-  let sId = "";
   let credit = "Thanks" + "&" + "Regards";
+  console.log(uwFirstName);
   let body = `Hi ${uwFirstName}, ${newLine}
   We have a Query Regarding the below Policy.${newLine}
   Policy Number : ${policyNumberList} ${newLine}
-  Submission ID : ${sId}${newLine}
+  Submission ID : ${submissionId}${newLine}
   Assured Name : ${assuredName}${newLine}
   Query Description : ${queryDescription}${newLine}
   ${credit}${newLine}${newLine}`;
   let hrefContent = `mailto:${underWriterEmailId}?subject=${subject}&cc=${cc}&body=${body}`;
-  console.log(hrefContent);
-  op.value = body;
+
   let uwEmailID = link.setAttribute("href", hrefContent);
 };
 
-function extractName(name) {}
+function getName(name) {
+  let fullName = name.split("@", 10)[0];
+  return (nameObj = {
+    firstName: fullName.split(".", 10)[0],
+    secondName: fullName.split(".", 10)[1],
+  });
+}
+
+function capitalizeName(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function getStringBetween(start, end, content) {
+  const result = content.match(new RegExp(start + "(.*)" + end));
+  const output = result[1];
+  return output;
+}
